7a26458f8e99b60f20926b90c7bd0145ea3ed3e8
Chattox <conradlineker@gmail.com>
2023-04-23 23:06:01 +0100

    wip: populate timers

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
 import { Timer } from './components/Timer';
+import { recipes } from './data/recipes';
 
 function App() {
   return (
     <div className="App">
+      {recipes.map((recipe) => (
+        <Timer label={recipe.label} img={recipe.img} duration={recipe.duration.cooked} />
+      ))}
       <Timer label="Test Timer" img={require('./data/images/fish.png')} duration={40} />
     </div>
   );
---
+++ src/data/recipes.ts
@@ -0,0 +1,98 @@
+export interface RecipeProps {
+  label: string;
+  img: string;
+  duration: {
+    undercooked: number;
+    cooked: number;
+    burnt: number;
+    fire: number;
+  };
+}
+
+const uncookableDuration = {
+  undercooked: 0,
+  cooked: 0,
+  burnt: 10,
+  fire: 310,
+};
+
+const meatDuration = {
+  undercooked: 50,
+  cooked: 60,
+  burnt: 120,
+  fire: 420,
+};
+
+const fishDuration = {
+  undercooked: 30,
+  cooked: 40,
+  burnt: 80,
+  fire: 380,
+};
+
+const trophyFishDuration = {
+  undercooked: 80,
+  cooked: 90,
+  burnt: 180,
+  fire: 480,
+};
+
+const monsterDuration = {
+  undercooked: 100,
+  cooked: 120,
+  burnt: 240,
+  fire: 540,
+};
+
+export const recipes: Array<RecipeProps> = [
+  {
+    label: 'Bait',
+    img: require('./images/fish.png'),
+    duration: { ...uncookableDuration },
+  },
+  {
+    label: 'Fruit',
+    img: require('./images/fish.png'),
+    duration: { ...uncookableDuration },
+  },
+  {
+    label: 'Snake',
+    img: require('./images/fish.png'),
+    duration: { ...meatDuration },
+  },
+  {
+    label: 'Chicken',
+    img: require('./images/fish.png'),
+    duration: { ...meatDuration },
+  },
+  {
+    label: 'Pig',
+    img: require('./images/fish.png'),
+    duration: { ...meatDuration },
+  },
+  {
+    label: 'Shark',
+    img: require('./images/fish.png'),
+    duration: { ...meatDuration },
+  },
+  {
+    label: 'Fish',
+    img: require('./images/fish.png'),
+    duration: { ...fishDuration },
+  },
+  {
+    label: 'Trophy Fish',
+    img: require('./images/fish.png'),
+    duration: { ...trophyFishDuration },
+  },
+  {
+    label: 'Megalodon',
+    img: require('./images/fish.png'),
+    duration: { ...monsterDuration },
+  },
+  {
+    label: 'Kraken',
+    img: require('./images/fish.png'),
+    duration: { ...monsterDuration },
+  },
+];
