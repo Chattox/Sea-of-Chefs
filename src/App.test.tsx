d47ec3150319062feb1949b5b5b8b50acccdc7e8
Chattox <conradlineker@gmail.com>
2023-04-23 23:34:14 +0100

    test(main): renders all timers

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { recipes } from './data/recipes';
 
-test('renders timer', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Test Timer/i);
-  expect(linkElement).toBeInTheDocument();
+describe('main page', () => {
+  it('renders timer', () => {
+    render(<App />);
+    const timerStartButton = screen.getByText(/Bait/i);
+    expect(timerStartButton).toBeInTheDocument();
+  });
+  it('renders all timers', () => {
+    render(<App />);
+    const timerLabels = recipes.land
+      .map((recipe) => recipe.label)
+      .concat(recipes.sea.map((recipe) => recipe.label));
+    timerLabels.forEach((label) => {
+      const labelElem = screen.getByText(label);
+      expect(labelElem).toBeInTheDocument();
+    });
+  });
 });
